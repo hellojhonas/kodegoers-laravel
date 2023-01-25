d82ba2d677b4bc79bd87c702ac3f58ae69270026
hellojhonas <jhonacenas0704@gmail.com>
2023-01-25 14:04:44 +0800

    putting laravel-react-starter in GitHub

---
+++ react/src/router.jsx
@@ -0,0 +1,60 @@
+import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
+import Notfound from "./views/NotFound";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
+import Dashboard from "./views/Dashboard";
+import { Navigate } from "react-router-dom";
+import UserForm from "./views/UserForm";
+
+const router = createBrowserRouter ([
+    {
+        path: '/',
+        element: <DefaultLayout/>,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to="/users"/>
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard/>
+            },
+            {
+                path: '/users',
+                element: <Users/>
+            },
+            {
+                path: '/users/new',
+                element: <UserForm key="userCreate"/>
+            },
+            {
+                path: '/users/:id',
+                element: <UserForm key="userUpdate"/>
+            }
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout/>,
+        children: [
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/signup',
+                element: <Signup/>
+            }
+        ]
+
+    },
+    {
+        path: '*',
+        element: <Notfound/>
+    }
+])
+
+export default router;
